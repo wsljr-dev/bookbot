3289dd1d3bdea6561e5de87517a4e957467e92a7
William Lewis Jr <wlewisjr586@proton.me>
2025-11-27 14:23:22 -0500

    Refactor stats functions to own file.

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
+from stats import count_words
+
+
 def get_book_text(filepath):
     book_text = ""
     with open(filepath) as f:
         book_text = f.read()
     return book_text
-
-
-def count_words(text):
-    words = text.split()
-    return len(words)
 
 
 def main():
