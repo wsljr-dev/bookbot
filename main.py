08b7e580683a7c05a8e6f8afb0c32caaba06e889
William Lewis Jr <wlewisjr586@proton.me>
2025-11-27 14:00:32 -0500

    Update main.py to read in book text.

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("greetings boots")
+def get_book_text(filepath):
+    book_text = ""
+    with open(filepath) as f:
+        book_text = f.read()
+    return book_text
+
+
+def main():
+    text = get_book_text("books/frankenstein.txt")
+    print(text)
+
+
+main()
